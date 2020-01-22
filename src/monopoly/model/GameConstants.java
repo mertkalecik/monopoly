52b46f59a97350d3610011ff30c4403b836e02f7
mertkalecik <mert.kalecik@hotmail.com>
2020-01-22 23:48:16 +0300

    Revert "Monopoly game init"
    This reverts commit 944e7e4145c93319423557dffb6560d32910a287.

--- src/monopoly/model/GameConstants.java
+++
@@ -1,7 +0,0 @@
-package monopoly.model;
-
-public final class GameConstants {
-	public static final int INITIAL_MONEY = 1500;
-	public static final int INITIAL_POSITION = 0;
-	public static final int JAIL_POSITION = 10;
-}
